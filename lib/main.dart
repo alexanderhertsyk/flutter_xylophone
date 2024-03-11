2e6c72ea73267edaf41d6413662c163575954672
Alexander <160505324+alexanderhertsyk@users.noreply.github.com>
2024-03-11 16:04:48 +0300

    Added sound assets. Added audioplayers package. Added Musical Note widget w/ logic/action but w/o styling.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const XylophoneApp());
@@ -10,9 +11,39 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              children: [
+                MusicalNote('C', 1),
+                MusicalNote('D', 2),
+                MusicalNote('E', 3),
+                MusicalNote('F', 4),
+                MusicalNote('G', 5),
+                MusicalNote('A', 6),
+                MusicalNote('B', 7),
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class MusicalNote extends StatelessWidget {
+  MusicalNote(this.noteName, this.order, {super.key});
+
+  final String noteName;
+  final int order;
+  final player = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () async {
+        await player.play(AssetSource('note$order.wav'));
+      },
+      child: Text(noteName),
     );
   }
 }
