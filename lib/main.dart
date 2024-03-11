630d5f77826b548c3585dedad82b0adb212f1831
Alexander <160505324+alexanderhertsyk@users.noreply.github.com>
2024-03-11 18:48:25 +0300

    Moved playing sound into separate function. Updated UI till the final version.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final notes = MusicNote.values.map((note) => NoteWidget(note)).toList();
+    final noteWidgets =
+        MusicNote.values.map((note) => NoteWidget(note)).toList();
 
     return MaterialApp(
       home: Scaffold(
@@ -16,7 +17,7 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: notes,
+            children: noteWidgets,
           ),
         ),
       ),
@@ -30,17 +31,24 @@ class NoteWidget extends StatelessWidget {
   final MusicNote note;
   final player = AudioPlayer();
 
+  Future<void> playSound(int noteNumber) async {
+    await player.play(AssetSource('note${note.number}.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () async {
-        await player.play(AssetSource('note${note.number}.wav'));
-      },
-      style: TextButton.styleFrom(
-        backgroundColor: note.color,
-        foregroundColor: Colors.white,
+    return Expanded(
+      child: TextButton(
+        onPressed: () async => await playSound(note.number),
+        style: TextButton.styleFrom(
+          backgroundColor: note.color,
+          foregroundColor: Colors.white,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(0.0)),
+          ),
+        ),
+        child: Text(note.name),
       ),
-      child: Text(note.name),
     );
   }
 }
