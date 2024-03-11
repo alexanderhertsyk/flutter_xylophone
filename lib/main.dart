00a7800deaaadaf4959c8c5fe9acef5dbfe7caec
Alexander <160505324+alexanderhertsyk@users.noreply.github.com>
2024-03-11 13:44:36 +0300

    Added assets. Updated main according to course.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const XylophonePage());
-}
+void main() => runApp(const XylophoneApp());
 
-class XylophonePage extends StatelessWidget {
-  const XylophonePage({super.key});
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Xylophone'),
+        body: SafeArea(
+          child: Container(),
         ),
       ),
     );
